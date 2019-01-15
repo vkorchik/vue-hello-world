58730dd15ea293a4086333d3361ffcdbe16aa21f
Vladimir Korchik <vladimir.korchik@fyber.com>
2019-01-15 12:41:00 +0100

    first

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,26 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TodoListItem
+      v-for="item in todoList"
+      :key="item.title"
+      :title="item.title"
+    ></TodoListItem>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import TodoListItem from './components/TodoList.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    TodoListItem
+  },
+  props: {
+    todoList: Array
   }
 }
 </script>
@@ -22,7 +32,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #232d36;
   margin-top: 60px;
 }
 </style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,13 @@
+<template>
+     <h3> {{ title }} </h3>
+</template>
+
+<script>
+export default {
+  name: 'TodoListItem',
+  props: {
+      title: String
+      }
+}
+</script>
+
